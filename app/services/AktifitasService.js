a1c27745f17d4cd8bb04cd91e4712902dc0907d6
Jong44 <mat.skak45@gmail.com>
2024-12-21 14:41:21 +0700

    update count transaksi

--- app/services/AktifitasService.js
+++ app/services/AktifitasService.js
@@ -41,7 +41,7 @@ const getAktifitasByUserId = async (id) => {
 }
 
 const getCountTransaksiByUserId = async (id) => {
-    const {data, error} = await supabase.from('aktifitas').select('*').eq('user_id', id);
+    const {data, error} = await supabase.from('aktifitas').select('*').eq('uuid', id).eq("activity_type", "transfer");
     if (error) throw new Error(error.message);
     return data.length;
 }
