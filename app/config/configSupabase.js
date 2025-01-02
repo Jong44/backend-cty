756457fcb7545166084f117b87b66a5321832b57
Jong44 <mat.skak45@gmail.com>
2025-01-03 01:52:59 +0700

    update dev

--- app/config/configSupabase.js
+++ app/config/configSupabase.js
@@ -1,7 +1,5 @@
 const { createClient } = require('@supabase/supabase-js')
 
-const dotenv = require('dotenv');
-dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
